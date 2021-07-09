926ee17f8545c7aa44af6933206f9966c14b5ea0
TurRil <lightscape2001@gmail.com>
2021-07-09 14:38:28 +0200

    TSUG-44: updated register.php

--- register.php
+++ register.php
@@ -5,7 +5,28 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-calendar-check-o",
     "short_name" => "One Button Studio",
     "description" => "This tool has a booking system, editing and managing of recordings made through using the One Button Studio.",
-    "messages" => array("launch")
-    );
-    
-    
+    "messages" => array("launch"),
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "tool_phase" => "emerging",
+    "source_url" => "https://github.com/cilt-uct/tsugi-one-button-studio",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+        /*
+        "store/screen-01.png",
+        "store/screen-02.png",
+        "store/screen-03.png"
+        */
+    )
+);
